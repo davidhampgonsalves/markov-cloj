af322636a509c1fced431a81c0de369043d07bd5
David Hamp-Gonsalves <david.hampgonsalves@goodguide.com>
2014-12-19 08:43:31 -0800

    word cleaning, chain building, etc

--- src/markov_text/core.clj
+++ src/markov_text/core.clj
@@ -5,30 +5,51 @@
 
 (defn cleanWord [word]
   (-> word 
-       .toLowerCase
-       .trim))
+    .toLowerCase
+    .trim
+    (.replaceAll "[,!-0-9*&.:;]+" "")))
 
 (defn isLastWord [word]
   (comment "is last word needs to handle words like N.S.A.")
-  (.endsWith word "."))
+  (-> word
+    .trim
+    (.matches "[!.?]$")))
 
 (defn createQueue []
   (conj (clojure.lang.PersistentQueue/EMPTY) "."))
 
-(defn nextSentence [scanner & {:keys [prefix chain] :or {prefix (createQueue) chain (hash-map)}}]
+(defn addToChain [chain prefix word]
+  (if-not (contains? @chain prefix)
+    (dosync
+      (alter chain assoc prefix (atom {word (atom 1)})))
+    (let [prefixChain (get @chain prefix)]
+      (if-not (contains? @prefixChain word)
+        (swap! prefixChain (assoc @prefixChain word (atom 1)))
+        (let [wordCount (get @prefixChain word)]
+          (swap! wordCount inc))))))
+
+(defn nextSentence [scanner & {:keys [prefix chain] :or {prefix (createQueue) chain (ref {})}}]
   "returns the next cleaned sentence"
   (if (.hasNext scanner)
-    (let [prefix (if (> (.size prefix) 2) (pop prefix) prefix)]
-      (let [w (.next scanner) prefix (conj prefix (cleanWord w))]
-        (println (clojure.string/join " " prefix))
-        (recur scanner {:prefix (if (isLastWord w) (createQueue) prefix) :chain chain})
-        ))
-    chain))
+    (let [rawWord (.next scanner) word (cleanWord rawWord)]
+      (if (.isEmpty word)
+        (recur scanner {:prefix prefix :chain chain})
+        (let [prefix (if (> (.size prefix) 2) (pop prefix) prefix)]
+          (addToChain chain (clojure.string/join " " prefix) word)
+          (recur scanner {:prefix (if (isLastWord rawWord) (createQueue) (conj prefix word)) :chain chain})
+        )))
+    @chain))
 
 
 (defn train [fileName chain]
   (let [sc (new Scanner (io/file fileName)) prefix (vector)]
-    (print (nextSentence sc))))
+    (let [chain (nextSentence sc)]
+      (println "chain size " (.size chain))
+      (println "sentence beginings " (get chain ""))
+      (println (keys chain))
+      (for [prefix (keys chain)]
+        (println prefix))
+      )))
 
 (defn -main
   "I run the show"
