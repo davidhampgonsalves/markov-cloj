a5639d8f52c49b46f0fddb1d06b76ca7e9717056
David Hamp-Gonsalves <david.hampgonsalves@goodguide.com>
2014-12-24 20:05:59 -0800

    revert added dependancy

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.cli "0.3.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot markov-text.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
