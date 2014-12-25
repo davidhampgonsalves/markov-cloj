29398e48319fc5b5441941cfcc3278b4d141a1c0
David Hamp-Gonsalves <david.hampgonsalves@goodguide.com>
2014-12-24 20:05:07 -0800

    prefix is defined at file level, handle cases(kind of) where we do not have a prefix match due to paragraph breaks

--- src/markov_text/core.clj
+++ src/markov_text/core.clj
@@ -5,6 +5,8 @@
   (:import  (java.util Scanner)
             (clojure.lang PersistentQueue) ))
 
+(def prefix-count 2)
+
 (defn clean-word [word]
   (comment "maybe I should match states without some punctuation but leave it in the output")
   (-> word 
@@ -24,40 +26,43 @@
 (defn build-prefix [prefix]
   (str/join " " prefix))
 
-(defn build-chain [scanner prefixCount & {:keys [prefix chain] :or {prefix (PersistentQueue/EMPTY) chain {}}}]
+(defn build-chain [scanner & {:keys [prefix chain] :or {prefix (PersistentQueue/EMPTY) chain {}}}]
   "returns the next cleaned sentence"
   (if (.hasNext scanner)
-    (let [rawWord (.next scanner) word (clean-word rawWord)]
-      (if (paragraph-boundry? rawWord)
-        (recur scanner prefixCount {:prefix (PersistentQueue/EMPTY) :chain chain})
-        (let [prefix (if (> (count prefix) prefixCount) (pop prefix) prefix)
+    (let [raw-word (.next scanner) word (clean-word raw-word)]
+      (if (paragraph-boundry? raw-word)
+        (recur scanner {:prefix (PersistentQueue/EMPTY) :chain chain})
+        (let [prefix (if (> (count prefix) prefix-count) (pop prefix) prefix)
               chain (update-in chain [(build-prefix prefix) word] (fnil inc 0))]
-          (recur scanner prefixCount {:prefix (conj prefix word) :chain chain})
+          (recur scanner {:prefix (conj prefix word) :chain chain})
        )))
     chain))
 
 (defn calculate-next-state [chain prefix]
   (let [states (seq (get chain (build-prefix prefix)))
-        stateCount (reduce (fn [c [state count]] 
+        state-count (reduce (fn [c [state count]] 
                   (+ c count)) 0 states)
-        stateIndex (rand-int stateCount)]
-      (loop [c 0 i 0]
-        (if (nil? (nth states i))
-          (do 
-            (println "\n\nERROR: states at " i " is nil. states: " states)
-            (println "prefix " (build-prefix prefix))))
-        (let [state (nth states i) c (+ c (last state))]
-          (if (>= c stateIndex)
-            (first state)
-            (recur c (inc i)))))))
+        state-index (rand-int state-count)]
+      (if (nil? states)
+        (do
+          (println "\n\nstates: " states)
+          (println "prefix " (build-prefix prefix))
+          nil)
+        (loop [c 0 i 0]
+          (let [state (nth states i) c (+ c (last state))]
+            (if (>= c state-index)
+              (first state)
+              (recur c (inc i))))))))
 
-(defn generate-sentence [chain prefixCount & 
-        {:keys [sentence prefix] :or {sentence (vector) prefix (PersistentQueue/EMPTY)}}]
-    (let [prefix (if (> (count prefix) prefixCount) (pop prefix) prefix)
+(defn generate-sentence [chain prefix & 
+        {:keys [sentence] :or {sentence []}}]
+    (let [prefix (if (> (count prefix) prefix-count) (pop prefix) prefix)
           word (calculate-next-state chain prefix)]
-      (if (last-word? word)
-        (conj sentence word)
-        (recur chain prefixCount {:sentence (conj sentence word) :prefix (conj prefix word)}))))
+      (if-not (nil? word)
+        (if (last-word? word)
+          (conj sentence word)
+          (recur chain (conj prefix word) {:sentence (conj sentence word)}))
+        nil)))
 
 (defn create-scanner [fileName]
   (.useDelimiter (new Scanner (io/file fileName)) "([\t ]+)|([\n]+)"))
@@ -65,9 +70,12 @@
 (defn -main
   "I run the show"
   [& args]
-  (let [prefixCount 2
-        sc (create-scanner (nth args 0))
-        chain (build-chain sc prefixCount)]
-    (dotimes [i 3]
-      (println (str/capitalize (str/join " " (generate-sentence chain prefixCount)))))))
+  (let [sc (create-scanner (nth args 0))
+        chain (build-chain sc)]
+
+      (loop [i 0 prefix (PersistentQueue/EMPTY)]
+        (let [sentence (generate-sentence chain prefix)]
+          (println (str/capitalize (str/join " " sentence)))
+          (if (< i 3)
+            (recur (inc i) (into (PersistentQueue/EMPTY) (take-last prefix-count sentence))))))))
 
