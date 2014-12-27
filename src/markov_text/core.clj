95b569840a1e38947abe1d270ff2d9aadeaa5bed
David Hamp-Gonsalves <david.hampgonsalves@goodguide.com>
2014-12-26 17:27:36 -0800

    major stuff works now

--- src/markov_text/core.clj
+++ src/markov_text/core.clj
@@ -7,12 +7,18 @@
 
 (def prefix-count 2)
 
+
+; When paragraph add next work to chain on its own?
+; when generating sentences don't store the prefix just take from sentence ending.
+
 (defn clean-word [word]
   (comment "maybe I should match states without some punctuation but leave it in the output")
   (-> word 
-    .toLowerCase
     .trim
-    (.replaceAll "[,0-9*&:;\"'-]+" "")))
+    (.replaceAll "[\"]+" "")))
+
+(defn conj-prefix [prefix word]
+  (conj prefix (str/upper-case word)))
 
 (defn last-word? [word]
   (comment "is last word needs to handle words like N.S.A.")
@@ -23,7 +29,7 @@
 (defn paragraph-boundry? [word]
   (.isEmpty word))
 
-(defn build-prefix [prefix]
+(defn prefix-str [prefix]
   (str/join " " prefix))
 
 (defn build-chain [scanner & {:keys [prefix chain] :or {prefix (PersistentQueue/EMPTY) chain {}}}]
@@ -33,20 +39,20 @@
       (if (paragraph-boundry? raw-word)
         (recur scanner {:prefix (PersistentQueue/EMPTY) :chain chain})
         (let [prefix (if (> (count prefix) prefix-count) (pop prefix) prefix)
-              chain (update-in chain [(build-prefix prefix) word] (fnil inc 0))]
-          (recur scanner {:prefix (conj prefix word) :chain chain})
+              chain (update-in chain [(prefix-str prefix) word] (fnil inc 0))]
+          (recur scanner {:prefix (conj-prefix prefix word) :chain chain})
        )))
     chain))
 
 (defn calculate-next-state [chain prefix]
-  (let [states (seq (get chain (build-prefix prefix)))
+  (let [states (seq (get chain (prefix-str prefix)))
         state-count (reduce (fn [c [state count]] 
                   (+ c count)) 0 states)
         state-index (rand-int state-count)]
       (if (nil? states)
         (do
           (println "\n\nstates: " states)
-          (println "prefix " (build-prefix prefix))
+          (println "prefix " (prefix-str prefix))
           nil)
         (loop [c 0 i 0]
           (let [state (nth states i) c (+ c (last state))]
@@ -61,21 +67,22 @@
       (if-not (nil? word)
         (if (last-word? word)
           (conj sentence word)
-          (recur chain (conj prefix word) {:sentence (conj sentence word)}))
+          (recur chain (conj-prefix prefix word) {:sentence (conj sentence word)}))
         nil)))
 
 (defn create-scanner [fileName]
   (.useDelimiter (new Scanner (io/file fileName)) "([\t ]+)|([\n]+)"))
 
 (defn -main
-  "I run the show"
+  "build markov chain based on supplied input and generate text from them."
   [& args]
-  (let [sc (create-scanner (nth args 0))
-        chain (build-chain sc)]
-
+  ;;build the chain for each input file in parallel and then merge them
+  (let [chains (pmap #(build-chain (create-scanner %)) args)
+        chain (apply merge-with #(merge-with + % %2) chains)]
       (loop [i 0 prefix (PersistentQueue/EMPTY)]
         (let [sentence (generate-sentence chain prefix)]
-          (println (str/capitalize (str/join " " sentence)))
+          (println (str/join " " sentence))
           (if (< i 3)
-            (recur (inc i) (into (PersistentQueue/EMPTY) (take-last prefix-count sentence))))))))
+            (recur (inc i) (into (PersistentQueue/EMPTY) (take-last prefix-count sentence))))))
+      (shutdown-agents)))
 
