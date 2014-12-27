1ca32ab71789a72e8354b0c7247106ef537aa160
David Hamp-Gonsalves <david.hampgonsalves@goodguide.com>
2014-12-27 11:57:35 -0800

    ignore paragraphs and determine initial state as a special case

--- src/markov_text/core.clj
+++ src/markov_text/core.clj
@@ -5,7 +5,7 @@
   (:import  (java.util Scanner)
             (clojure.lang PersistentQueue) ))
 
-(def prefix-count 2)
+(def prefix-count 3)
 
 
 ; When paragraph add next work to chain on its own?
@@ -26,22 +26,18 @@
     .trim
     (.matches ".+[!.?]$")))
 
-(defn paragraph-boundry? [word]
-  (.isEmpty word))
-
 (defn prefix-str [prefix]
-  (str/join " " prefix))
+  "Join prefix into a string"
+  (str/upper-case (str/join " " prefix)))
 
 (defn build-chain [scanner & {:keys [prefix chain] :or {prefix (PersistentQueue/EMPTY) chain {}}}]
-  "returns the next cleaned sentence"
+  "Build a Markov chain based on the provided scanner."
   (if (.hasNext scanner)
-    (let [raw-word (.next scanner) word (clean-word raw-word)]
-      (if (paragraph-boundry? raw-word)
-        (recur scanner {:prefix (PersistentQueue/EMPTY) :chain chain})
-        (let [prefix (if (> (count prefix) prefix-count) (pop prefix) prefix)
-              chain (update-in chain [(prefix-str prefix) word] (fnil inc 0))]
-          (recur scanner {:prefix (conj-prefix prefix word) :chain chain})
-       )))
+    (let [raw-word (.next scanner) word (clean-word raw-word)
+          prefix-full (> (count prefix) prefix-count)
+          prefix (if prefix-full (pop prefix) prefix)
+          chain (if prefix-full (update-in chain [(prefix-str prefix) word] (fnil inc 0)) chain)]
+        (recur scanner {:prefix (conj-prefix prefix word) :chain chain}))
     chain))
 
 (defn calculate-next-state [chain prefix]
@@ -49,29 +45,33 @@
         state-count (reduce (fn [c [state count]] 
                   (+ c count)) 0 states)
         state-index (rand-int state-count)]
-      (if (nil? states)
-        (do
-          (println "\n\nstates: " states)
-          (println "prefix " (prefix-str prefix))
-          nil)
-        (loop [c 0 i 0]
-          (let [state (nth states i) c (+ c (last state))]
-            (if (>= c state-index)
-              (first state)
-              (recur c (inc i))))))))
+    (loop [c 0 i 0]
+      (let [state (nth states i) c (+ c (last state))]
+        (if (>= c state-index)
+          (first state)
+          (recur c (inc i)))))))
+
+(defn initial-state [chain]
+  "Randomly select an initial state"
+  (let [prefixes (keys chain)
+        prefix-index (rand-int (count prefixes))
+        prefix (str/split (nth prefixes prefix-index) #" ")]
+    (if (-> prefix first (.endsWith "."))
+      (into PersistentQueue/EMPTY (flatten [(next prefix) (calculate-next-state chain prefix)]))
+      (recur chain))))
 
 (defn generate-sentence [chain prefix & 
         {:keys [sentence] :or {sentence []}}]
-    (let [prefix (if (> (count prefix) prefix-count) (pop prefix) prefix)
-          word (calculate-next-state chain prefix)]
-      (if-not (nil? word)
+    (if (nil? prefix)
+      (let [prefix (initial-state chain)] 
+        (recur chain prefix {:sentence (into [] prefix)}))
+      (let [word (calculate-next-state chain prefix)]
         (if (last-word? word)
           (conj sentence word)
-          (recur chain (conj-prefix prefix word) {:sentence (conj sentence word)}))
-        nil)))
+          (recur chain (conj-prefix (pop prefix) word) {:sentence (conj sentence word)})))))
 
 (defn create-scanner [fileName]
-  (.useDelimiter (new Scanner (io/file fileName)) "([\t ]+)|([\n]+)"))
+  (.useDelimiter (new Scanner (io/file fileName)) "([\t \n]+)"))
 
 (defn -main
   "build markov chain based on supplied input and generate text from them."
@@ -79,7 +79,7 @@
   ;;build the chain for each input file in parallel and then merge them
   (let [chains (pmap #(build-chain (create-scanner %)) args)
         chain (apply merge-with #(merge-with + % %2) chains)]
-      (loop [i 0 prefix (PersistentQueue/EMPTY)]
+      (loop [i 0 prefix nil]
         (let [sentence (generate-sentence chain prefix)]
           (println (str/join " " sentence))
           (if (< i 3)
--- test/markov_text/core_test.clj
+++ test/markov_text/core_test.clj
@@ -1,16 +1,28 @@
 (ns markov-text.core-test
   (:require [clojure.test :refer :all]
-            [markov-text.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+            [markov-text.core :refer :all]
+            [clojure.string :as str])
+  (:import  (clojure.lang PersistentQueue)))
 
 (deftest last-word-check 
   (testing "with spaces"
-    (is (isLastWord " asdf. ")))
+    (is (last-word? " asdf. ")))
   (testing "with ?" 
-    (is (isLastWord "asdf?"))))
+    (is (last-word? "asdf?"))))
 
-(deftest getNextWord
-  )
+(deftest initial-state-test
+  (testing "orders state corectly"
+    (is (=
+      (first (initial-state {"THIS. IS A" {"test" 1}})) 
+      "IS")))
+  (testing "picks the right one"
+    (is (= 
+      (last (initial-state {"ONE TWO. THREE" {"FOUR" 1} "TWO. THREE FOUR" {"FIVE" 1}}))
+      "FIVE"))))
+
+(deftest generate-sentence-test 
+  (testing "generates sentence"
+    (let [chain {"THE START" {"HAS" 1}
+                 "START HAS" {"ENDED." 1}}]
+      (is (= (generate-sentence chain (into PersistentQueue/EMPTY ["THE" "START"]))
+             ["THE" "START" "HAS" "ENDED."])))))
